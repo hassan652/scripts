e04f71e638b93555a9014d4a9e9e962b4aaee0c0
hassanmahmood652 <hassanmahmood652@gmail.com>
2021-02-28 12:34:27 +0100

    Live webcam stream

---
+++ Python Programs GVT/webcamlive.py
@@ -0,0 +1,12 @@
+#Capture video from the webcam
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("live", frame)
+    if cv2.waitKey(1) & 0XFF == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
